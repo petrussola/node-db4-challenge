49b3ae9abec554d52f0f32fea11ee1a41b185152
Pere <pere.solaclaver@gmail.com>
2019-11-07 15:43:54 +0000

    Refactor index and server.js

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
-const helmet = require('helmet');
-
-const server = express();
-
-server.use(helmet());
-server.use(cors());
-server.use(express.json());
-
-server.get("/", (req, res) => {
-  res.status(200).json({ message: `Hello from dummy endpoint` });
-});
+const server = require("./server");
 
 const port = process.env.PORT || 4000;
 
