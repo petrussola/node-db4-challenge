ad1c658c8d8dd1d2e0d1bbbabeef6b22a6102f1c
Pere <pere.solaclaver@gmail.com>
2019-11-07 16:18:50 +0000

    Create query to get shopping list and end point

--- data/helpers/recipesModel.js
+++ data/helpers/recipesModel.js
@@ -1,17 +1,22 @@
 const db = require("../db-config");
 
 module.exports = {
-  getRecipes
-//   getShoppingList,
-//   getInstructions
+  getRecipes,
+  getShoppingList
+  //   getInstructions
 };
 
 function getRecipes() {
-    return db("recipes");
+  return db("recipes");
 }
 
 function getShoppingList(recipe_id) {
-  
+  return db
+    .select("i.ingredient_name", "ir.ingredient_quantity", "i.ingredient_unit")
+    .from("ingredientsRecipes as ir")
+    .join("ingredients as i", "ir.ingredient_id", "i.id")
+    .join("recipes as r", "ir.receipe_id", "r.id")
+    .where("ir.receipe_id", recipe_id);
 }
 
 function getInstructions(recipe_id) {}
--- data/helpers/recipesRouter.js
+++ data/helpers/recipesRouter.js
@@ -14,4 +14,17 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:id/shoppingList", (req, res) => {
+  const { id } = req.params;
+  Recipes.getShoppingList(id)
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(error => {
+      res
+        .status(404)
+        .json({ message: `Could not find shopping list: ${error.message}` });
+    });
+});
+
 module.exports = router;
