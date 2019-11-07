e94c492c6ada68af4929eb8f9d6b721b473f1364
Pere <pere.solaclaver@gmail.com>
2019-11-07 14:10:17 +0000

    create knexfile.js and delete staging and production

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const helmet = require('helmet');
 
 const server = express();
 
+server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
