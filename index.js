4b1e30faf66dd3d0610ec6ec490a453d5c4da094
Pere <pere.solaclaver@gmail.com>
2019-11-07 13:57:11 +0000

    Create boilerplate code for express app with dummy endpoint

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const server = express();
+
+server.use(cors());
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.status(200).json({ message: `Hello from dummy endpoint` });
+});
+
+const port = process.env.PORT || 4000;
+
+server.listen(port, () => {
+  console.log(`\n*** Listening on port ${port}***\n`);
+});
